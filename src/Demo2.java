249db2618124077730dc02ed4f319e55656d18de
Anurag <anurag1342001@gmail.com>
2022-09-27 23:11:28 +0530

    ok

---
+++ src/Demo2.java
@@ -0,0 +1,20 @@
+public class Demo2
+{
+    public static void main(String []args)
+    {
+        int var=10;
+        System.out.println(var);
+        int var$=30;
+        System.out.println(var$);
+        int va$r=40;
+        System.out.println(va$r);
+        int $var=20;
+        System.out.println($var);
+        int _var=50;
+        System.out.println(_var);
+        int var_=60;
+        System.out.println(var_);
+        int va_r=70;
+        System.out.println(va_r);
+    }
+}
